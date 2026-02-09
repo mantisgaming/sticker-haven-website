a0e45a66f35e98930e35089fa1ff447b2014bb31
Max Allen <mjbgtaad56@gmail.com>
2026-02-08 20:07:36 -0500

    Started layout

---
+++ src/routes/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { resolve } from "$app/paths";
+
+</script>
+
+<header>
+    <a class="title" href={resolve("/")}>Sticker Haven</a>
+    <nav>
+        <ul>
+            <a href={resolve("/about")}>About</a>
+        </ul>
+    </nav>
+</header>
+
+<style lang="scss">
+    
+</style>
