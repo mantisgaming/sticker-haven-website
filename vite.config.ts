7d3efde18d18646b1f28f74970f3efdc88142ea6
Max Allen <mjbgtaad56@gmail.com>
2026-03-12 14:42:27 -0400

    Added cloudflare vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
-export default defineConfig({ plugins: [sveltekit(), devtoolsJson()] });
+export default defineConfig({ plugins: [sveltekit(), cloudflare({
+    viteEnvironment: {
+        name: 'staging'
+    },
+}), devtoolsJson()] });
